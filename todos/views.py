58a16b44c5b02c11023b888174294961a05b61a8
GyuReeKim <starkim96@naver.com>
2019-10-02 15:56:15 +0900

    index 완성

--- todos/views.py
+++ todos/views.py
@@ -3,7 +3,11 @@ from .models import Todo
 
 # Create your views here.
 def index(request): # request는 인자의 이름
-    return render(request, 'index.html') # render는 python에서 html을 읽을 수 있도록 한다.
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'index.html', context) # render는 python에서 html을 읽을 수 있도록 한다.
 
 def new(request):
     return render(request, 'new.html')
