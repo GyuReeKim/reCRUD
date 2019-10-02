2c23f3d6fa6ccbb14627c6b50e7d354dcdba48f3
GyuReeKim <starkim96@naver.com>
2019-10-02 14:48:35 +0900

    모델 작성

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model): # models 안의 Model을 상속받는다.
+    title = models.CharField(max_length=50)
+    due_date = models.DateField()
